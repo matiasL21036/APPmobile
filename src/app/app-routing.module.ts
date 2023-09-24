1a071dec44a24e190c410b45a015a3570c05c58a
m1p <matia.lopezv@duocuc.cl>
2023-09-24 18:01:15 -0300

    main funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -39,47 +38,6 @@ const routes: Routes = [
   {
     path: 'qr',
     loadChildren: () => import('./pages/qr/qr.module').then( m => m.QRPageModule)
-=======
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'inicio',
-    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
-  },
-  {
-    path: 'recuperar',
-    loadChildren: () => import('./pages/recuperar/recuperar.module').then( m => m.RecuperarPageModule)
-  },
-  {
-    path: 'horario',
-    loadChildren: () => import('./pages/horario/horario.module').then( m => m.HorarioPageModule)
-  },
-  {
-    path: 'escanear',
-    loadChildren: () => import('./pages/escanear/escanear.module').then( m => m.EscanearPageModule)
-  },
-  {
-    path: 'asignaturaprofesor',
-    loadChildren: () => import('./pages/asignaturaprofesor/asignaturaprofesor.module').then( m => m.AsignaturaprofesorPageModule)
-  },
-  {
-    path: 'profesor',
-    loadChildren: () => import('./pages/profesor/profesor.module').then( m => m.ProfesorPageModule)
-  },
-  {
-    path: 'qr',
-    loadChildren: () => import('./pages/qr/qr.module').then( m => m.QRPageModule)
   },
   {
     path: 'asistencia',
@@ -88,17 +46,12 @@ const routes: Routes = [
   {
     path: 'asistenciaprofe',
     loadChildren: () => import('./pages/asistenciaprofe/asistenciaprofe.module').then( m => m.AsistenciaprofePageModule)
-  },  {
-    path: 'horarioprofesor',
-    loadChildren: () => import('./pages/horarioprofesor/horarioprofesor.module').then( m => m.HorarioprofesorPageModule)
->>>>>>> 27333ea81a58f3aea0a12810982bf274995f40a9
   },
   {
-    path: 'registro', // Nueva ruta para la pestaña de registro
-    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+    path: 'horarioprofesor',
+    loadChildren: () => import('./pages/horarioprofesor/horarioprofesor.module').then( m => m.HorarioprofesorPageModule)
   },
 
-<<<<<<< HEAD
 ];
 
 @NgModule({
@@ -108,14 +61,7 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-=======
-];
 
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
->>>>>>> 27333ea81a58f3aea0a12810982bf274995f40a9
+
+
+
