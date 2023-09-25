82ef504bba6654aeea0c82d339fa678e66e5890d
Felipe Valdebenito <fe.valdebenito@duocuc.cl>
2023-09-25 13:13:39 -0300

    Últimos cambios

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  nombreUsuario: string = ''; // Inicializa las propiedades
+  correo: string = '';
+  contrasena: string = '';
+  confirmarContrasena: string = '';
 
-  constructor() { }
+  constructor(private alertController: AlertController) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async registrarUsuario() {
+    if (this.validarCampos()) {
+      // Lógica para registrar al usuario
+      // Aquí puedes agregar el código para enviar los datos al servidor, por ejemplo.
+
+      // Muestra una alerta de registro exitoso
+      const alert = await this.alertController.create({
+        header: 'Éxito',
+        message: '¡Registro exitoso!',
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+    } else {
+      // Muestra una alerta de error si la validación falla
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Por favor, completa todos los campos correctamente.',
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+    }
   }
 
+  validarCampos(): boolean {
+    // Realiza la validación de los campos aquí
+    // Puedes agregar tus propias validaciones, por ejemplo, verificar que las contraseñas coincidan, etc.
+    
+    if (this.nombreUsuario.trim() === '' || this.correo.trim() === '' || this.contrasena.trim() === '' || this.confirmarContrasena.trim() === '') {
+      return false; // Al menos un campo está vacío
+    }
+
+    if (this.contrasena !== this.confirmarContrasena) {
+      return false; // Las contraseñas no coinciden
+    }
+
+
+    return true; // Todos los campos son válidos
+  }
 }
