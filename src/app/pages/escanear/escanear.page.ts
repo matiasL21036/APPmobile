e9a0c400f6f78618b9016b6fbe4bf2f062a79de8
m1p <matia.lopezv@duocuc.cl>
2023-11-17 14:20:40 -0300

    scanner

--- src/app/pages/escanear/escanear.page.ts
+++ src/app/pages/escanear/escanear.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
 @Component({
   selector: 'app-escanear',
   templateUrl: './escanear.page.html',
@@ -7,21 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EscanearPage implements OnInit {
 
-  constructor() { }
+  isSupported = false;
+  barcodes: Barcode[] = [];
+
+  constructor(private alertController: AlertController) {}
 
   ngOnInit() {
+    BarcodeScanner.isSupported().then((result) => {
+      this.isSupported = result.supported;
+    });
   }
 
-  mostrarImagenYGuardarMensaje() {
-    // Muestra la imagen
-    const imageToDisplay = document.getElementById('imageToDisplay');
-    imageToDisplay.style.display = 'block';
-    
-    // Almacena el mensaje en el localStorage
-    const mensaje = "Ubicación: Paicavi 3280, Hora del Dispositivo: " + new Date().toLocaleString();
-    localStorage.setItem('mensaje', mensaje);
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+  }
 
-    // Puedes mostrar una alerta o realizar otra acción después de guardar los datos
-    //alert('Imagen mostrada y mensaje guardado en localStorage.');
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 }
