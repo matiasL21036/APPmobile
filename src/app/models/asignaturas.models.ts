d9593e4c36c01f745a039bf5447633c5a8a29a65
Felipe Valdebenito <fe.valdebenito@duocuc.cl>
2023-11-25 21:23:58 -0300

    Asignaturas base de datos

---
+++ src/app/models/asignaturas.models.ts
@@ -0,0 +1,9 @@
+export interface asignatura {
+  id: string;
+  nom_asignatura: string;
+  horainicio: string;
+  horatermino: string;
+  seccion: string;
+  Presente: boolean;
+  asistencia: number;
+}
--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,26 +1,30 @@
 import { Injectable, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoadingController, ToastController, ToastOptions } from '@ionic/angular';
+import {
+  LoadingController,
+  ModalController,
+  ModalOptions,
+  ToastController,
+  ToastOptions,
+} from '@ionic/angular';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UtilsService {
-
   loadingCtrl = inject(LoadingController);
   toastCtrl = inject(ToastController);
+  modalCtrl = inject(ModalController);
   router = inject(Router);
 
   // loading
   loading() {
-    return this.loadingCtrl.create({ spinner: 'crescent' })
+    return this.loadingCtrl.create({ spinner: 'crescent' });
   }
-
 
   async presentToast(opts?: ToastOptions) {
     const toast = await this.toastCtrl.create(opts);
     toast.present();
-
   }
 
   // enrutar
@@ -30,17 +34,24 @@ export class UtilsService {
 
   // guardar elemento en localstorage
   saveInLocalStorage(key: string, value: any) {
-    return localStorage.setItem(key, JSON.stringify(value))
+    return localStorage.setItem(key, JSON.stringify(value));
   }
-
 
   // obtener elemento desde localstorage
   getFromLocalStorage(key: string) {
-    return JSON.parse(localStorage.getItem(key))
+    return JSON.parse(localStorage.getItem(key));
   }
 
+  // Modal
+  async presentModal(opts: ModalOptions) {
+    const modal = await this.modalCtrl.create(opts);
+    await modal.present();
 
+    const { data } = await modal.onWillDismiss();
+    if (data) return data;
+  }
 
-
-
+  dismissModal(data?: any) {
+    return this.modalCtrl.dismiss(data);
+  }
 }
